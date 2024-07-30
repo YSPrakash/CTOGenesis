541b001b2f6f45e39b650bb2d8f5239ecdfa19ba
YSPrakash <147952322+YSPrakash@users.noreply.github.com>
2024-07-30 20:56:54 +0530

    Add files via upload

---
+++ hack3.py
@@ -0,0 +1,51 @@
+import base64
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part, FinishReason
+import vertexai.preview.generative_models as generative_models
+# python .\hack2.py C:\Users\Avina\Downloads\a2ticwy4.png
+
+import base64
+import sys
+import os
+
+#os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./key.json"
+def image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+    return encoded_string
+
+
+def generate(image_path):
+  image1 = Part.from_data(
+    mime_type="image/png",
+    data=image_to_base64(image_path))
+  vertexai.init(project="hackathon-431005", location="us-central1")
+  model = GenerativeModel(
+    "gemini-1.5-flash-001",
+  )
+  responses = model.generate_content(
+      [image1, """Please help to generate the source code to build given architecture diagram in python"""],
+      generation_config=generation_config,
+      #safety_settings=safety_settings,
+      stream=True,
+  )
+  return responses
+#   for response in responses:
+#     print(response.text, end="")
+
+
+
+generation_config = {
+    "max_output_tokens": 8192,
+    "temperature": 1,
+    "top_p": 0.95,
+}
+
+safety_settings = {
+    generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+}
+
+#generate(image_path)
